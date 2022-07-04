eeead4e745e6b6ba9bb30c729411f4a10d9646ab
Xavier Mod <xavmod@tuimedia.com>
2022-07-04 22:05:21 +0100

    Add entities

--- maestro-api/src/config/orm.config.prod.ts
+++ maestro-api/src/config/orm.config.prod.ts
@@ -1,5 +1,11 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Song } from 'src/songs/song.entity';
+import { SongPart } from 'src/songs/songPart.entity';
+import { Track } from 'src/tracks/track.entity';
+import { TrackRequest } from 'src/tracks/trackRequest.entity';
+import { User } from 'src/users/user.entity';
+import { Like } from 'typeorm';
 
 export default registerAs(
   'orm.config',
@@ -11,7 +17,7 @@ export default registerAs(
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     // Contains all entities from the project
-    entities: [],
+    entities: [Song, SongPart, User, Track, Like, TrackRequest],
     // Synchronize automatically updates the DB schema when changing entities.
     // ! Should not be true in prod
     synchronize: false,
